048ffdaae201f125f870685aa3d9e08b55314e96
chenzhizs <chenzhi@qq.com>
2022-03-09 16:52:10 +0900

    Initial Commit

---
+++ tset_IBOutlet_IBAction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tset_IBOutlet_IBAction
+//
+//  Created by chenzhizs on 2022/03/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
