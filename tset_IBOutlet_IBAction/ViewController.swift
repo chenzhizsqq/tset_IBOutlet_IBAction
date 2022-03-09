3f428a23f2387bcf21ac8bfbbc81f2dfc5b6054b
chenzhizs <chenzhi@qq.com>
2022-03-09 16:58:02 +0900

    test IBOutlet

--- tset_IBOutlet_IBAction/ViewController.swift
+++ tset_IBOutlet_IBAction/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var testText: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        testText.text = "test ok"
     }
-
-
+    
 }
 
